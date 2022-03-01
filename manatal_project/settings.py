f8e8e392e9c869a4c2a9b691b9f737cb6e23edda
RemyOstyn <ostyn.r@gmail.com>
2022-03-01 18:31:06 +0700

    add CSRF_TRUSTED_ORIGINS

--- manatal_project/settings.py
+++ manatal_project/settings.py
@@ -28,6 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['djangoapi.team-cactus.com','127.0.0.1']
 
+CSRF_TRUSTED_ORIGINS = ["https://djangoapi.team-cactus.com", "https://www.djangoapi.team-cactus.com"]
+
 
 # Application definition
 
